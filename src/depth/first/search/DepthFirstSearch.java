255a9623dfcf5250a658e58e8556c6cc3ee0c019
Maher Zaidoune <maherzaidoune@gmail.com>
2017-12-30 13:18:57 +0100

    implements Algo logic

--- src/depth/first/search/DepthFirstSearch.java
+++ src/depth/first/search/DepthFirstSearch.java
@@ -1,5 +1,8 @@
 package depth.first.search;
 
+import java.util.ArrayList;
+import java.util.Stack;
+
 /**
  *
  * @author maher
@@ -17,10 +20,62 @@ package depth.first.search;
  */
 
 public class DepthFirstSearch {
-
+    Node startNode;
+    Node goalNode;
+    ArrayList<Node> done;
     
+    public DepthFirstSearch(Node startNode,Node goalNode){
+        this.startNode = startNode;
+        this.goalNode = goalNode;
+    }
+    public boolean search(){
+        if (this.startNode.equals(this.goalNode)) {
+            System.out.println("Goal is just the start Node");
+            return true;
+        }
+        Stack<Node> lifo = new Stack();
+        done = new ArrayList<>();
+        lifo.add(startNode);
+        while(!lifo.isEmpty()){
+            Node current = lifo.pop();
+            if(current.equals(goalNode)){
+                printDone();
+                return true;
+            }else{
+                done.add(current);
+                lifo.addAll(current.getChilden());
+            }
+        }
+        return false;
+    }
+    
+    public void printDone(){
+        int steps = 0;
+        for(Node n : done){
+            System.out.println(++steps + " : " +n.nodeName);
+            
+        }
+    }
+
     public static void main(String[] args) {
+        // Testing ..
+        // Binary tree , A,B,C,D,E,F,G 
+        Node I = new Node("I",null,null);
+        Node H = new Node("H",null,null);
+        Node E = new Node("E",null,null);
+        Node F = new Node("F",null,null);
+        Node G = new Node("G",null,null);
+        Node D = new Node("D",H,I);
+        Node C = new Node("C",F,G);
+        Node B = new Node("B",D,E);
+        Node A = new Node("A",B,C);
         
+        DepthFirstSearch rrdl = new DepthFirstSearch(A,E);
+        if(rrdl.search()){
+            System.out.println("success");
+        }else{
+            System.out.println("nope");
+        }
     }
     
 }
